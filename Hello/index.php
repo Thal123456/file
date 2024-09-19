4eec391f908244d1bafb0db7050399d361fe8368
Jazzknow <jaspermiclat62@gmail.com>
2024-09-19 18:02:43 +0800

    index.php

--- Hello/index.php
+++ Hello/index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Time Calculator</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
+    <div>HELLO WORLD</div>
     <div class="container">
         <h1>Time Calculator</h1>
         <form id="timeForm">
@@ -27,4 +30,5 @@
 
     <script src="script.js"></script>
 </body>
+
 </html>
